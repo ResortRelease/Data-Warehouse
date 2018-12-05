7acad6444fdccc5af856e9e7f0ed5102b994e5c2
VPL <VPL@Admins-iMac-2.local>
2018-12-05 17:58:35 -0500

    🧼🧽

--- RR-Mailing.py
+++ RR-Mailing.py
@@ -22,8 +22,32 @@ experian = pd.read_csv(
     encoding="ISO-8859-1",
 )
 
+# Import latest deals file
+contacts = s3.get_object(Bucket=BUCKET_NAME, Key='DUMP/Deals1.csv')
+contacts = pd.read_csv(
+    contacts['Body'],
+    index_col=False,
+    low_memory=False,
+    encoding="ISO-8859-1",
+)
+
+# Drop extra fields
+contacts = contacts[['dealid', 'ClientName', 'StreetAddress', 'ZipCode', 'City', 'State', 'HomePhone', 'SecondaryPhone', 'EmailAddress', 'status']]
+
+# Only working with lead cloud status
+contacts = contacts[contacts["status"].str.contains('1|2|5a')==True]
+contacts = contacts.loc[-contacts['status'].isin([False])]
+all_lead = contacts['dealid'].unique().tolist()
+
+
+# ========= Experian ==========
+
+# Only dealids with that match status
+experian = experian.loc[experian['DealId'].isin(all_lead)]
+
 # Remove rows without address
 experian = experian.dropna(subset=['RA RETURNED A1'])
+experian = experian.dropna(subset=['EMAILADDRESS'])
 
 # Lowercase email and address
 experian['EMAILADDRESS'] = experian['EMAILADDRESS'].str.lower()
@@ -34,67 +58,6 @@ unique_email = experian['EMAILADDRESS'].unique().tolist()
 unique_phone = experian['PHONE'].unique().tolist()
 unique_address = experian['RA RETURNED A1'].unique().tolist()
 
-# Import latest deals file
-contacts = s3.get_object(Bucket=BUCKET_NAME, Key='DUMP/Deals1.csv')
-
-# Load it into dataframe without saving
-contacts = pd.read_csv(
-    contacts['Body'],
-    index_col=0,
-    low_memory=False,
-    encoding="ISO-8859-1",
-)
-
-# County info
-# contacts['county'] = contacts['ZipCode'].apply(rr_fun.add_county)
-
-contacts = contacts[['ClientName', 'StreetAddress', 'ZipCode', 'City', 'State', 'HomePhone', 'SecondaryPhone', 'EmailAddress', 'status']]
-
-contacts['Full Name'] = contacts['ClientName'].apply(rr_fun.clean_client_name)
-contacts['First Name'], contacts['Last Name'] = contacts['Full Name'].str.split(' ', 1).str
-
-# Lowercase email and address
-contacts['EmailAddress'] = contacts['EmailAddress'].str.lower()
-contacts['StreetAddress'] = contacts['StreetAddress'].str.lower()
-
-# Drop rows with blank address
-contacts = contacts.dropna(subset=['StreetAddress'])
-
-# A lot of dirt
-contacts = contacts[contacts["StreetAddress"].str.contains('will provide|Refused')==False]
-contacts = contacts.loc[-contacts['StreetAddress'].isin([False])]
-
-# Clean phone number to only digits
-contacts['HomePhone'] = contacts['HomePhone'].apply(rr_fun.clean_number)
-contacts['SecondaryPhone'] = contacts['SecondaryPhone'].apply(rr_fun.clean_number)
-
-print("Entire", contacts.shape[0])
-
-# Remove rows that match the list of phone # and emails
-contacts = contacts.loc[-contacts['HomePhone'].isin(unique_phone)]
-print("Home", contacts.shape[0])
-
-contacts = contacts.loc[-contacts['SecondaryPhone'].isin(unique_phone)]
-print("Secondary", contacts.shape[0])
-
-contacts = contacts.loc[-contacts['EmailAddress'].isin(unique_email)]
-print("Email", contacts.shape[0])
-
-contacts = contacts.loc[-contacts['StreetAddress'].isin(unique_address)]
-print("Address", contacts.shape[0])
-
-contacts = contacts[contacts["status"].str.contains('0b|1|2|5a')==True]
-contacts = contacts.loc[-contacts['status'].isin([False])]
-
-print("Only 0b,1,2,5a", contacts.shape[0])
-print(len(contacts['StreetAddress'].unique().tolist()))
-print(len(contacts['HomePhone'].unique().tolist()))
-print(len(contacts['EmailAddress'].unique().tolist()))
-
-# Sanity test
-contacts = contacts[['EmailAddress', 'HomePhone', 'ClientName', 'First Name', 'Last Name', 'StreetAddress', 'City', 'State', 'ZipCode']]
-contacts.to_csv('export-addresses.csv')
-
 # Clean name and addresses
 experian['RA RETURNED FIRST NAME'] = experian['RA RETURNED FIRST NAME'].str.title()
 experian['RA RETURNED LAST NAME'] = experian['RA RETURNED LAST NAME'].str.title()
@@ -102,5 +65,72 @@ experian['RA RETURNED A1'] = experian['RA RETURNED A1'].str.title()
 experian['RA RETURNED A2'] = experian['RA RETURNED A2'].str.title()
 experian['RA RETURNED CITY'] = experian['RA RETURNED CITY'].str.title()
 
-experian = experian[['EMAILADDRESS', 'PHONE', 'NAME', 'RA RETURNED FIRST NAME', 'RA RETURNED LAST NAME', 'RA RETURNED A1', 'RA RETURNED CITY', 'RA RETURNED STATE', 'RA RETURNED ZIP']]
+experian = experian[['EMAILADDRESS', 'PHONE', 'NAME', 'RA RETURNED FIRST NAME', 'RA RETURNED LAST NAME', 'RA RETURNED A1', 'RA RETURNED CITY', 'RA RETURNED STATE', 'RA RETURNED ZIP', 'DealId']]
 experian.to_csv('export-experian.csv')
+
+list_of_experian = experian['DealId'].unique().tolist()
+print("Experian", len(list_of_experian))
+
+
+# ============ CRM ============ 
+
+# Clean names
+contacts['Full Name'] = contacts['ClientName'].apply(rr_fun.clean_client_name)
+contacts['First Name'], contacts['Last Name'] = contacts['Full Name'].str.split(' ', 1).str
+
+# Lowercase email and address
+contacts['EmailAddress'] = contacts['EmailAddress'].str.lower()
+contacts['StreetAddress'] = contacts['StreetAddress'].str.lower()
+
+print("Entire:", contacts.shape[0])
+# Drop leads that already exist in experian
+contacts = contacts.loc[-contacts['dealid'].isin(list_of_experian)]
+print("Entire minus experian:", contacts.shape[0])
+
+# Drop rows with blank address
+contacts = contacts.dropna(subset=['StreetAddress'])
+print("No Street Address:", contacts.shape[0])
+
+# A lot of dirt
+contacts = contacts[contacts["StreetAddress"].str.contains('will provide|Refused')==False]
+contacts = contacts.loc[-contacts['StreetAddress'].isin([False])]
+print("Without Dirt:", contacts.shape[0])
+
+# Eh? No canada
+contacts = contacts[contacts["City"].str.contains('Canada|CANADA|canada')==False]
+contacts = contacts[contacts["State"].str.contains('Canada|CANADA|canada')==False]
+contacts = contacts.loc[-contacts['City'].isin([False])]
+contacts = contacts.loc[-contacts['State'].isin([False])]
+print("No Canada:", contacts.shape[0])
+
+# Clean phone number to only digits
+contacts['HomePhone'] = contacts['HomePhone'].apply(rr_fun.clean_number)
+contacts['SecondaryPhone'] = contacts['SecondaryPhone'].apply(rr_fun.clean_number)
+
+# Remove rows that match the list of phone # and emails
+contacts = contacts.loc[-contacts['HomePhone'].isin(unique_phone)]
+print("Home:", contacts.shape[0])
+
+contacts = contacts.loc[-contacts['SecondaryPhone'].isin(unique_phone)]
+print("Secondary:", contacts.shape[0])
+
+contacts = contacts.loc[-contacts['EmailAddress'].isin(unique_email)]
+print("Email:", contacts.shape[0])
+
+# Lowercase email and address
+contacts['StreetAddress'] = contacts['StreetAddress'].str.lower()
+
+# Drop address that show up in Experian
+contacts = contacts.loc[-contacts['StreetAddress'].isin(unique_address)]
+
+# Title the streets
+contacts['StreetAddress'] = contacts['StreetAddress'].str.title()
+print("Address:", contacts.shape[0])
+
+print("Only 1,2,5a:", contacts.shape[0])
+print(len(contacts['StreetAddress'].unique().tolist()))
+print(len(contacts['HomePhone'].unique().tolist()))
+print(len(contacts['EmailAddress'].unique().tolist()))
+
+contacts = contacts[['EmailAddress', 'HomePhone', 'ClientName', 'First Name', 'Last Name', 'StreetAddress', 'City', 'State', 'ZipCode']]
+contacts.to_csv('export-addresses.csv')
